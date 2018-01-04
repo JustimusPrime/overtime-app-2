de7d5f9e0639c5d39ba030697b99410fc7672d6b
JustimusPrime <hirejustin@yahoo.com>
2018-01-04 14:50:20 -0700

    Implemented the ability to delete post records

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,15 +19,16 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
 
-    it 'has a list of Posts' do
+    it 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
       post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
     end
+  end
 
-    describe 'new' do
-      it 'has a link from the homepage' do
+  describe 'new' do
+    it 'has a link from the homepage' do
       visit root_path
 
       click_link("new_post_from_nav")
@@ -35,45 +36,49 @@ describe 'navigate' do
     end
   end
 
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
   end
 
   describe 'creation' do
-    before do 
+    before do
       visit new_post_path
     end
 
-    it 'has a new form that can be reached' do  
+    it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'can be created from new form page' do
-      visit new_post_path
-
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
     end
 
-    it 'will have a user associated it' do 
+    it 'will have a user associated it' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
-
-    end 
+    end
   end
 
-  describe 'edit' do 
-      before do
-        @post = FactoryGirl.create(:post)
-      end
+  describe 'edit' do
+    before do
+      @post = FactoryGirl.create(:post)
+    end
 
-      it 'can be reached by clicking edit on index page' do
-        visit posts_path
+    it 'can be reached by clicking edit on index page' do
+      visit posts_path
 
       click_link("edit_#{@post.id}")
       expect(page.status_code).to eq(200)
@@ -87,8 +92,6 @@ describe 'navigate' do
       click_on "Save"
 
       expect(page).to have_content("Edited content")
-
     end
   end
 end
-
