d63f98be68c548dbda8d3d6856b5456983277ea7
JustimusPrime <hirejustin@yahoo.com>
2018-01-18 15:50:35 -0700

    integrated valiations and defaults for audit log attributes

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
   factory :audit_log do
-    user nil
+    user
     status 1
-    start_date "2018-01-18"
-    end_date "2018-01-18"
+    start_date (Date.today - 6.days)
+    end_date nil
   end
 end
+
