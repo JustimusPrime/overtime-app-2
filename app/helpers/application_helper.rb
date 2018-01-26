7257ed11e8626755be0304f0a213bc29e16ba1e7
JustimusPrime <hirejustin@yahoo.com>
2018-01-26 12:07:22 -0700

    updated nav to remove components not needed for admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,14 @@ def active?(path)
   "active" if current_page?(path)
 end
 
+def employee?
+  current_user.type == "Employee"
+end
+
+def admin?
+  admin_types.include?(current_user.type)
+end
+
 def status_label status
   status_span_generator status
 end
